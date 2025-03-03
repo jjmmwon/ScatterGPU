b5d431e52b491d71aca97263690f2933243593e5
jjmmwon <jmw1790@gmail.com>
2025-03-03 18:11:40 +0900

    initialize project with Vite, TypeScript, and basic configuration files

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // src/ 전체를 alias로 지정
+    },
+  },
+});
